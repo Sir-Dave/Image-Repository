c1058039cd617680ce7fbb70ae898e99b8f754c7
Abiola David <davidolanrewaju2000@gmail.com>
2021-09-16 16:01:37 +0100

    Initialized project

---
+++ src/main/kotlin/com/sirdave/imagerepository/ImageRepositoryApplication.kt
@@ -0,0 +1,11 @@
+package com.sirdave.imagerepository
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageRepositoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImageRepositoryApplication>(*args)
+}
